b5662fbf6f4ced64ece7f91ab7a9df786ce95508
kasrakoushan <kasra.koushan@gmail.com>
2015-01-17 21:25:18 -0500

    This is the class containing all the strategies.

---
+++ StrategyList.py
@@ -0,0 +1,7 @@
+class StrategyList:
+    def basic_strategy(game_state):
+        
+        move = f(game_state)
+        #move is SOME function of the given game state        
+        
+        return move
