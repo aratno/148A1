cb8f802ef4700b9056ba956b27a4315085e97152
kasrakoushan <kasra.koushan@gmail.com>
2015-01-17 21:22:45 -0500

    This is the Player class.

---
+++ Player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+    
+    def strategy(game_state):
+        
+        move = f(game_state)
+        #move is SOME function of the given game state
+        
+        return move
