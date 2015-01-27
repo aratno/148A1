52326475e4b789efa4139181559b50fd5b423765
Kasra Koushan <Kasra@user65-14.vicres.utoronto.ca>
2015-01-27 16:50:06 -0500

    Made the strategy class generic

--- StrategyList.py
+++ StrategyList.py
@@ -1,10 +1,2 @@
-from random import randint
-
 class StrategyList:
-    def basic_strategy(game):
-        """(Game) -> int
-        
-        Take a game object and produce a random choice from the options.
-        """
-
-        return randint(1, len(game.options))
+    """A generic strategy class."""
