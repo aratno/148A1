a8297c737606ac29ab2e4796940b9752c22d04c9
aratno <aratno@gmail.com>
2015-01-27 22:49:50 -0500

    Last commit of the evening.

--- game_view.py
+++ game_view.py
@@ -11,15 +11,12 @@ def print_title(game):
     print('+' + '-'*len(game.title) + '+')
     print('|' + game.title + '|')
     print('+' + '-'*len(game.title) + '+')
-    print('Type help({}) for help.'.format(game.title))
 
 def print_instructions(game):
     """
 
     """
-    width = 2 + len(game.title)
-    for i in range(width):
-        print
+    print('\n' + game.instructions + '\n') 
 
 def print_turn(game):
     """
@@ -27,5 +24,14 @@ def print_turn(game):
     """
     print('~ {}\'s turn'.format(game.players[0]))
 
+def print_state(game):
+    """
+
+    """
+    print('The game state is:\n{}'.format(game.game_state))
+
 print_title(a)
+print_instructions(a)
+#while a.state > 0:
+print_target(a)
 print_turn(a)
