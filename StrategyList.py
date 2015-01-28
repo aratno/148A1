72eefca57842768f2f2a32e5438e43fb0d570872
kasrakoushan <kasra.koushan@gmail.com>
2015-01-28 18:41:25 -0500

    Edited class description

--- StrategyList.py
+++ StrategyList.py
@@ -1,7 +1,7 @@
 class StrategyList:
     """A generic strategy class.
     
-    This class does not have __init__, __str__, __repr__, and __eq__ methods because it is simply a superclass for subclasses that include the strategies.
+    This class does not have __init__, __str__, __repr__, and __eq__ methods because it is simply a superclass for subclasses that contain the strategies.
     """
     
     def default_strategy(game):
